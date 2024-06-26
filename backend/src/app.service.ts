3611e3a4ed661d695ae1c5d52c76288e34e77673
Kolotylo Kyrylo <kolot102@gmail.com>
2024-06-26 15:24:41 +0300

    feat(auth): add prisma service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  getHello() {
+    return this.prisma.user.findMany();
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT);
 }
 bootstrap();

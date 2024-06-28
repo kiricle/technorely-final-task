6e7bb06aef5b1fae43b6f0b055b093ee510d58c5
Kolotylo Kyrylo <kolot102@gmail.com>
2024-06-28 17:49:37 +0300

    feat(auth): register form layout

--- client/src/components/RegisterForm/RegisterForm.tsx
+++ client/src/components/RegisterForm/RegisterForm.tsx
@@ -1,12 +1,48 @@
+import { Button } from '../../ui/Button/Button';
 import { Input } from '../../ui/Input/Input';
+import styles from './RegisterForm.module.scss';
 
 export const RegisterForm = () => {
     return (
-        <form>
-            <Input
-                content="Email"
-                name="email"
-            />
+        <form className={styles.form}>
+            <h2 className={styles.heading}>Sign Up</h2>
+            <div className={styles.content}>
+                <Input
+                    content="Email"
+                    name="email"
+                />
+                <Input
+                    content="Password"
+                    name="password"
+                />
+                <Input
+                    content="First name"
+                    name="firstName"
+                />
+                <Input
+                    content="last name"
+                    name="lastName"
+                />
+                <Input
+                    content="nickname"
+                    name="nickname"
+                />
+                <Input
+                    content="position"
+                    name="position"
+                />
+                <Input
+                    content="phone number"
+                    name="phoneNumber"
+                    type="tel"
+                />
+            </div>
+            <Button
+                style={{ width: '100%' }}
+                appearance="primary"
+            >
+                Sign up
+            </Button>
         </form>
     );
 };
